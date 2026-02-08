b19e5e8ea55b85488fee89ae7b1d3728b76e120a
Aditya Shekhar <aditya.shekhar2004@gmail.com>
2026-02-08 20:51:10 +0530

    [26] [Time Beats: 100.00%] [Memory Beats: 42.40%] - LeetPush

---
+++ Remove-Duplicates-from-Sorted-Array.py
@@ -0,0 +1,14 @@
+1class Solution:
+2    def removeDuplicates(self, nums: list[int]) -> int:
+3        if not nums:
+4            return 0
+5
+6        k = 1  
+7
+8        for i in range(1, len(nums)):
+9            if nums[i] != nums[i - 1]:
+10                nums[k] = nums[i]
+11                k += 1
+12
+13        return k
+14
